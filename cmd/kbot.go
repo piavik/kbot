29fc595c50a4cf56be9e3aaac4390eeb67d8309d
piavik <piavik@gmail.com>
2025-05-12 17:47:39 +0200

    v.1.0.4 - readme, start with 'start' parameter

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -19,7 +19,7 @@ var (
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:     "kbot",
+	Use:     "start",
 	Aliases: []string{"start"},
 	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
